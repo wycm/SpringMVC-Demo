e36ae52487f5606a7b320ee296ffd5b1bd0bcadb
wycm <1057160387@qq.com>
2018-02-01 23:44:39 +0800

    update

---
+++ web/src/main/java/com/wy/controller/AmbiguousTestController.java
@@ -0,0 +1,24 @@
+//package com.wy.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import javax.servlet.http.HttpServletRequest;
+//
+//@Controller
+//@RequestMapping("/AmbiguousTest")
+//public class AmbiguousTestController {
+//    @RequestMapping(value = "/test1")
+//    @ResponseBody
+//    public String test1(){
+//        return "method test1";
+//    }
+//
+//    @RequestMapping(value = "/test1")
+//    @ResponseBody
+//    public String test2(){
+//        return "method test2";
+//    }
+//}
