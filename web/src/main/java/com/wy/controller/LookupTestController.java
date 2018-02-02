cc6dd48970bcd770d1487bbe88972ea334a5b9a7
wycm <1057160387@qq.com>
2018-02-02 20:27:59 +0800

    update

---
+++ web/src/main/java/com/wy/controller/LookupTestController.java
@@ -0,0 +1,50 @@
+package com.wy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/LookupTest")
+public class LookupTestController {
+
+    @RequestMapping(value = "/test1", method = RequestMethod.GET)
+    @ResponseBody
+    public String test1(){
+        return "method test1";
+    }
+
+    @RequestMapping(value = "/test1", headers = "Referer=https://www.baidu.com")
+    @ResponseBody
+    public String test2(){
+        return "method test2";
+    }
+
+    @RequestMapping(value = "/test1", params = "id=1")
+    @ResponseBody
+    public String test3(){
+        return "method test3";
+    }
+
+    @RequestMapping(value = "/*")
+    @ResponseBody
+    public String test4(){
+        return "method test4";
+    }
+
+    @RequestMapping(value = "/test5", method = RequestMethod.GET)
+    @ResponseBody
+    public String test5(){
+        return "method test5";
+    }
+
+
+    @RequestMapping(value = "/test5", method = {R})
+    @ResponseBody
+    public String test6(){
+        return "method test6";
+    }
+}
