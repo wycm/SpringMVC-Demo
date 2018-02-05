6efcb0a5992cb754ecaaecb39a5638d466e342a9
wycm <1057160387@qq.com>
2018-02-05 21:00:09 +0800

    update

---
+++ web/src/main/java/com/wy/controller/ParameterBindTestController.java
@@ -0,0 +1,24 @@
+package com.wy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/ParameterBind")
+public class ParameterBindTestController {
+
+    @ResponseBody
+    @RequestMapping("/test1")
+    public String test1(int id){
+        System.out.println(id);
+        return "test1";
+    }
+
+    @ResponseBody
+    @RequestMapping("/test2")
+    public String test2(User u){
+        System.out.println(u);
+        return "test2";
+    }
+}
